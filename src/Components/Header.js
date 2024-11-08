278ce1ea4b736d7ca0c369e96224355a61027a06
Preeti <prity.dudi@gmail.com>
2024-11-08 20:14:05 +0530

    header update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,49 +1,34 @@
-// src/components/Header.js
-
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaBook, FaComments, FaChalkboardTeacher, FaUser, FaVideo, FaQuestion, FaCog, FaPoll, FaClipboardList, FaTimes, FaBars } from 'react-icons/fa';
+import { FaHome, FaBook, FaComments, FaChalkboardTeacher, FaUser, FaQuestion, FaCog, FaTimes, FaBars } from 'react-icons/fa';
 import '../Styles/HeaderStyles.css';
 
 const navLinks = [
     { to: '/', icon: <FaHome />, text: 'Home' },
     { to: '/articles', icon: <FaBook />, text: 'Articles' },
-    { to: '/chat', icon: <FaComments />, text: 'Chat' },
     { to: '/courses', icon: <FaChalkboardTeacher />, text: 'Courses' },
-    { to: '/explore', icon: <FaPoll />, text: 'Explore' },
     { to: '/forum', icon: <FaComments />, text: 'Forum' },
     { to: '/profile', icon: <FaUser />, text: 'Profile' },
     { to: '/quiz', icon: <FaQuestion />, text: 'Quiz' },
     { to: '/settings', icon: <FaCog />, text: 'Settings' },
-    { to: '/survey', icon: <FaClipboardList />, text: 'Survey' },
-    { to: '/videos', icon: <FaVideo />, text: 'Videos' },
 ];
 
 const Header = () => {
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
     const closeMenu = () => setIsMenuOpen(false);
-
 
     return (
         <header className="header">
             <div className="header-logo">
                 <h1>Educacy</h1>
             </div>
-            {!isMenuOpen && <button className="menu-toggle" onClick={toggleMenu}>
-                <FaBars />
-            </button>}
+            <button className="menu-toggle" onClick={toggleMenu}>
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
             <nav className={`header-nav ${isMenuOpen ? 'open' : 'closed'}`}>
                 <ul>
-                    <li>
-                        {isMenuOpen &&
-                            <button className="menu-toggle" onClick={toggleMenu}>
-                                <FaTimes />
-                            </button>}
-
-                    </li>
                     {navLinks.map((link, index) => (
                         <li key={index}>
                             <Link to={link.to} onClick={closeMenu} className="nav-link">
@@ -57,7 +42,5 @@ const Header = () => {
         </header>
     );
 }
-
-
 
 export default Header;
